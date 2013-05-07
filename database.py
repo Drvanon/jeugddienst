8c744be3acec9922cf46723c2cd8ff5ffda7b390
rob <robbiedorstijn@gmail.com>
2013-05-07 11:39:07 +0000

    Write forum get&post and poll get

--- database.py
+++ database.py
@@ -7,35 +7,39 @@ engine = create_engine('sqlite:///jeugddienst.db')
 
 class Poll(Base):
     __tablename__ = 'polls'
-    
+
     id = Column(Integer, primary_key=True)
     title = Column(String)
-    
+    option1 = Column(String)
+    option2 = Column(String)
+    option3 = Column(String)
+    option4 = Column(String)
+
 class Vote(Base):
     __tablename__ = 'votes'
-    
+
     id = Column(Integer, primary_key=True)
     option = Column(Integer)
     poll_id = Column(Integer, ForeignKey('polls.id'))
     poll = relationship("Poll", backref=backref('polls', order_by=id))
     post_date = Column(DateTime)
-    
+
 class Forum(Base):
     __tablename__ = 'fora'
 
     id = Column(Integer, primary_key=True)
     title = Column(String)
-    
+
 class Reaction(Base):
     __tablename__ = 'reactions'
-    
+
     id = Column(Integer, primary_key=True)
     content = Column(String)
     name = Column(String)
     forum_id = Column(Integer, ForeignKey('fora.id'))
     forum = relationship("Fora", backref=backref('reactions', order_by=id))
     post_date = Column(DateTime)
-    
+
 Base.metadata.create_all(engine)
 
 Session = sessionmaker()
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from bottle import run, route, jinja2_view as view, request
+from bottle import run, route, jinja2_view as view, request, default_app
 from bottel import post, get
 from database import *
 
@@ -8,11 +8,42 @@ session = Session()
 @view('home.html')
 def home():
     return {}
-    
+
+@get('/forum/<forumname>')
+@view('forum.html')
+def forum(forumname):
+    forum = session.query(Forum).filter_by(title=forumname).first()
+    if forum:
+        return {'title': forum.title,
+                'reactions': forum.reactions
+                }
+    else:
+        return {'error': 'Forum not found.'}
+
 @post('/forum/<forumname>')
 def forum_post(forumname):
     if forumname in session.query(Forum).all():
         name = request.forms.get('name')
         reaction = request.forms.get('reaction')
+        new_reaction = Reaction(reaction, name)
+        session.add(new_reaction)
+        session.commit()
     else:
-        return {'error':'That forum does not exist'}
+        return {'error':'Forum not found.'}
+
+@get('/poll/<pollname>')
+@view('poll.html')
+def poll(pollname):
+    poll = session.query(Poll).filter_by(title=pollname).first()
+    if forum:
+        return {'title': poll.title,
+                'option1': poll.option1,
+                'option2': poll.option2,
+                'option3': poll.option3,
+                'option4': poll.option4,
+                }
+    else:
+        return {'error': 'Forum not found.'}
+
+if __name___ == '__main__': run(debug=True)
+else: application = default_app()
