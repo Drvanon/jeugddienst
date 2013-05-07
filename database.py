6492b4bf8066b96d8b008cf235e171c1f1276bd4
ddorstijn <dannydorstijn1997@gmail.com>
2013-05-07 08:52:34 +0200

    Fix databases to fit both polls and fora

--- database.py
+++ database.py
@@ -5,21 +5,37 @@ from sqlalchemy.orm import sessionmaker, relationship, backref
 Base = declarative_base()
 engine = create_engine()
 
-class Stelling(Base):
-    __tablename__ = 'stellingen'
+class Poll(Base):
+    __tablename__ = 'polls'
     
     id = Column(Integer, primary_key=True)
     content = Column(String)
     
-class Stem(Base):
-    __tablename__ = 'stemmen'
+class Vote(Base):
+    __tablename__ = 'votes'
     
     id = Column(Integer, primary_key=True)
-    stelling_id = Column(Integer, ForeignKey('stellingen.id'))
-    stelling = relationship("Stelling", backref=backref('stemmen', order_by=id)
+    option = Column(Integer)
+    poll_id = Column(Integer, ForeignKey('polls.id'))
+    poll = relationship("Poll", backref=backref('polls', order_by=id)
+    post_date = Column(DateTime)
     
+class Forum(Base):
+    __tablename__ = 'fora'
 
-
+    id = Column(Integer, primary_key=True)
+    content = Column(String)
+    
+class Reaction(Base):
+    __tablename__ = 'reactions'
+    
+    id = Column(Integer, primary_key=True)
+    content = Column(String)
+    name = Column(String)
+    forum_id = Column(Integer, ForeignKey('fora.id'))
+    forum = relationship("Fora", backref=backref('reactions', order_by=id)
+    post_date = Column(DateTime)
+    
 Base.metadata.create_all(engine)
 
 Session = sessionmaker()
