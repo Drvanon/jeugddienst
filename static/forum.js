f2aa67a7a41748131736da070ce1e5b3f4ff563b
ddorstijn <dannydorstijn1997@gmail.com>
2013-05-08 17:12:14 +0200

    Fix the stupid error of not running code

--- static/forum.js
+++ static/forum.js
@@ -1,8 +1,9 @@
-$(document).ready(function () {
-    $("#reactionForm").submit(function (e) {
+$(document).on( "pageinit", ".forumpage", function() {
+    alert('shits happening');
+    $(".reactionForm").on('submit', function (e) {
         e.preventDefault();
         var form_content;
-        form_content = $('#reactionForm').serialize();
+        form_content = $('.reactionForm').serialize();
         $.post(window.location.pathname,
             form_content, 
             function () {
--- static/home.js
+++ static/home.js
@@ -1,1 +1,15 @@
-// Put your custom code here
+$( "body" ).on( "changepage", function( event ) {
+    alert('shits happening');
+    $(".reactionForm").on('submit', function (e) {
+        e.preventDefault();
+        var form_content;
+        form_content = $('.reactionForm').serialize();
+        $.post(window.location.pathname,
+            form_content, 
+            function () {
+                window.location ='/';
+            }
+        );
+        return false;
+    });
+});
